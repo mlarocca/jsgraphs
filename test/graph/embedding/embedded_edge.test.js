248246121e60952bcc00c90398b79c3be78e22b1
mlarocca <marcellolarocca@gmail.com>
2020-03-28 00:48:30 +0100

    embedded edge's tests

--- test/graph/embedding/embedded_edge.test.js
+++ test/graph/embedding/embedded_edge.test.js
@@ -3,16 +3,13 @@ import 'mjs-mocha';
 import EmbeddedEdge from '../../../src/graph/embedding/embedded_edge.js';
 import EmbeddedVertex from '../../../src/graph/embedding/embedded_vertex.js';
 
-import Graph from '../../../src/graph/graph.js';
-
-import { choose } from '../../../src/common/array.js';
-import { consistentStringify } from '../../../src/common/strings.js';
 import { testAPI, testStaticAPI } from '../../utils/test_common.js';
 import { ERROR_MSG_INVALID_ARGUMENT, ERROR_MSG_INVALID_LABEL } from '../../../src/common/errors.js';
 
 import chai from "chai";
 import should from "should";
-import Point from '../../../src/geometric/point.js';
+import Point2D from '../../../src/geometric/point2d.js';
+import Vertex from '../../../src/graph/vertex.js';
 
 const expect = chai.expect;
 
@@ -23,85 +20,103 @@ describe('EmbeddedEdge API', () => {
   });
 
   it('# Class should have a static fromJson method', function () {
-    let staticMethods = [];
+    const staticMethods = ['fromJson', 'fromJsonObject'];
     testStaticAPI(EmbeddedEdge, staticMethods);
   });
 
   it('# Object\'s interface should be complete', () => {
-    let embeddedGraph = new EmbeddedEdge(new EmbeddedVertex("a", new Point(1,1)), new EmbeddedVertex("b", new Point(2,2)));
-    let methods = ['constructor', 'toJson', 'toString', 'toSvg', 'clone'];
-    let attributes = [];
-    testAPI(embeddedGraph, attributes, methods);
+    const embeddedEdge = new EmbeddedEdge(new EmbeddedVertex("a", new Point2D(1, 1)), new EmbeddedVertex("b", new Point2D(2, 2)));
+    const methods = ['constructor', 'toJsonObject', 'toString', 'toSvg', 'clone'];
+    const attributes = ['isDirected', 'arcControlDistance'];
+    testAPI(embeddedEdge, attributes, methods);
   });
 });
 
 describe('EmbeddedEdge Creation', () => {
-  describe('# Parameters', () => {
-    let point = new Point(1,2,3);
-    describe('# 1st argument (mandatory)', () => {
-      // it('should throw when label is null or undefined', () => {
-      //   (() => new EmbeddedEdge(null, point)).should.throw(ERROR_MSG_INVALID_ARGUMENT('Vertex()', 'label', null));
-      //   (() => new EmbeddedEdge(undefined, point)).should.throw(ERROR_MSG_INVALID_ARGUMENT('Vertex()', 'label', undefined));
-      // });
+  const point = new Point2D(1, 2);
+  const u = new EmbeddedVertex('u', point);
+  const v = new EmbeddedVertex('v', point);
 
-      // it('should throw when source is not a Vertex', () => {
-      //   (() => new EmbeddedEdge(new Map(), point)).should.throw(ERROR_MSG_INVALID_LABEL('Vertex()', 'label', new Map()));
-      //   (() => new EmbeddedEdge(new Set(), point)).should.throw(ERROR_MSG_INVALID_LABEL('Vertex()', 'label', new Set()));
-      // });
+  describe('# Parameters', () => {
+    describe('# 1st argument (mandatory)', () => {
+      it('should throw when source is null or undefined', () => {
+        (() => new EmbeddedEdge(null, v)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'source', null));
+        (() => new EmbeddedEdge(undefined, v)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'source', undefined));
+      });
+
+      it('should throw when source is not an EmbeddedVertex', () => {
+        (() => new EmbeddedEdge(new Map(), v)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'source', new Map()));
+        (() => new EmbeddedEdge(new Set(), v)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'source', new Set()));
+        const w = new Vertex('simple vertex');
+        (() => new EmbeddedEdge(w, v)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'source', w));
+      });
+
+      it('should set source correctly when an EmbeddedVertex is passed', () => {
+        (() => new EmbeddedEdge(u, v)).should.not.throw();
+        const e = new EmbeddedEdge(u, v);
+        e.source.equals(u).should.be.true();
+      });
     });
 
     describe('# 2nd argument (mandatory)', () => {
+      it('should throw when destination is null or undefined', () => {
+        (() => new EmbeddedEdge(u, null)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'destination', null));
+        (() => new EmbeddedEdge(u, undefined)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'destination', undefined));
+      });
+
+      it('should throw when destination is not an EmbeddedVertex', () => {
+        (() => new EmbeddedEdge(u, new Map())).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'destination', new Map()));
+        (() => new EmbeddedEdge(u, new Set())).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'destination', new Set()));
+        const w = new Vertex('simple vertex');
+        (() => new EmbeddedEdge(u, w)).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'destination', w));
+      });
+
+      it('should set destination correctly when an EmbeddedVertex is passed', () => {
+        (() => new EmbeddedEdge(u, v)).should.not.throw();
+        const e = new EmbeddedEdge(u, v);
+        e.destination.equals(v).should.be.true();
+      });
     });
 
-    describe('# 3rd argument (optional)', () => {
-      // it('should default to weight=1', () => {
-      //   new EmbeddedEdge(2, 1).weight.should.eql(1);
-      // });
+    describe('# isDirected (optional)', () => {
+      it('should default to false', () => {
+        new EmbeddedEdge(u, v).isDirected().should.be.false();
+      });
 
-      // it('should throw if it\'s not a number', () => {
-      //   (() => new EmbeddedEdge(3, [], { weight: 'r' })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'weight', 'r'));
-      //   (() => new EmbeddedEdge(3, [], { weight: [] })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'weight', []));
-      //   (() => new EmbeddedEdge(3, [], { weight: false })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'weight', false));
-      //   (() => new EmbeddedEdge(3, [], { weight: {} })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'weight', {}));
-      // });
+      it('should throw if it\'s not a boolean', () => {
+        (() => new EmbeddedEdge(u, v, { isDirected: 'r' })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'isDirected', 'r'));
+        (() => new EmbeddedEdge(u, v, { isDirected: [] })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'isDirected', []));
+        (() => new EmbeddedEdge(u, v, { isDirected: 12 })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'isDirected', 12));
+        (() => new EmbeddedEdge(u, v, { isDirected: {} })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'isDirected', {}));
+      });
 
-      // it('should NOT throw with numbers', () => {
-      //   (() => new EmbeddedEdge(2, [], { weight: 1 })).should.not.throw();
-      //   (() => new EmbeddedEdge(2, [], { weight: 0.1 })).should.not.throw();
-      //   (() => new EmbeddedEdge(2, [], { weight: -145 })).should.not.throw();
-      // });
-
-      // it('should NOT throw with numeric strings', () => {
-      //   (() => new EmbeddedEdge(2, [], { weight: '1' })).should.not.throw();
-      //   (() => new EmbeddedEdge(2, [], { weight: '3.1415' })).should.not.throw();
-      //   (() => new EmbeddedEdge(2, [], { weight: '-145' })).should.not.throw();
-      //   (() => new EmbeddedEdge(2, [], { weight: '1e12' })).should.not.throw();
-      // });
+      it('should set it correctly when boolean values are passed', () => {
+        new EmbeddedEdge(u, v, { isDirected: true }).isDirected().should.be.true();
+        new EmbeddedEdge(u, v, { isDirected: false }).isDirected().should.be.false();
+      });
     });
 
-    describe('# 4th argument (optional)', () => {
-      // it('should default to label=undefined', () => {
-      //   expect(new EmbeddedEdge(2, 1).label).to.be.undefined;
-      //   expect(new EmbeddedEdge(2, 1, { weight: -1 }).label).to.be.undefined;
-      // });
+    describe('# arcControlDistance (optional)', () => {
+      it('should default to DEFAULT_EDGE_LOOP_RADIUS, for loops', () => {
+        new EmbeddedEdge(u, u).arcControlDistance.should.eql(EmbeddedEdge.DEFAULT_EDGE_LOOP_RADIUS);
+        new EmbeddedEdge(v, v).arcControlDistance.should.eql(EmbeddedEdge.DEFAULT_EDGE_LOOP_RADIUS);
+      });
 
-      // it('should not throw if label is null', () => {
-      //   expect(() => new EmbeddedEdge(3, [], { label: null })).not.to.throw();
-      //   expect(new EmbeddedEdge(3, [], { label: null }).label).to.be.undefined;
-      // });
+      it('should default to DEFAULT_EDGE_BEZIER_CONTROL_DISTANCE, for non-loops', () => {
+        new EmbeddedEdge(u, v).arcControlDistance.should.eql(EmbeddedEdge.DEFAULT_EDGE_BEZIER_CONTROL_DISTANCE);
+      });
 
-      // it('should not throw if label is a string', () => {
-      //   expect(() => new EmbeddedEdge(3, [], { label: '' })).not.to.throw();
-      //   expect(() => new EmbeddedEdge(3, [], { label: 'some string' })).not.to.throw();
-      //   expect(() => new EmbeddedEdge(3, [], { label: 'unicode! ☺☺☻' })).not.to.throw();
-      // });
+      it('should throw if it\'s not numberic', () => {
+        (() => new EmbeddedEdge(u, v, { arcControlDistance: 'r' })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'arcControlDistance', 'r'));
+        (() => new EmbeddedEdge(u, v, { arcControlDistance: [] })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'arcControlDistance', []));
+        (() => new EmbeddedEdge(u, v, { arcControlDistance: {} })).should.throw(ERROR_MSG_INVALID_ARGUMENT('EmbeddedEdge()', 'arcControlDistance', {}));
+      });
 
-      // it('should throw with other types', () => {
-      //   (() => new EmbeddedEdge('2', 3, { label: 3 })).should.throw();
-      //   (() => new EmbeddedEdge([], true, { label: true })).should.throw();
-      //   (() => new EmbeddedEdge({}, [], { label: [12, 2] })).should.throw();
-      //   (() => new EmbeddedEdge(false, {}, { label: {} })).should.throw();
-      // });
+      it('should set it correctly when numeric values are passed', () => {
+        new EmbeddedEdge(u, v, { arcControlDistance: 11 }).arcControlDistance.should.eql(11);
+        new EmbeddedEdge(u, v, { arcControlDistance: -11 }).arcControlDistance.should.be.eql(-11);
+        new EmbeddedEdge(u, v, { arcControlDistance: '3.14' }).arcControlDistance.should.eql(3.14);
+      });
     });
   });
 });
@@ -111,38 +126,46 @@ describe('Attributes', () => {
 });
 
 describe('Methods', () => {
+  const point = new Point2D(1, 2);
+  const u = new EmbeddedVertex(['u'], point);
+  const v = new EmbeddedVertex({ name: ['v'] }, point);
+
   describe('toJson()', () => {
     it('# should return a valid json', () => {
-      // labels.forEach(label => {
-      //   let source = choose(sources);
-      //   let dest = choose(sources);
-      //   let weight = Math.random();
-      //   let e = new EmbeddedEdge(source, dest, { label: label, weight: weight });
-      //   expect(() => JSON.parse(e.toJson())).not.to.throw();
-      // });
-    });
-
-    it('# should stringify the fields consistently', () => {
-      // let e = new EmbeddedEdge(0, '1', { label: 'label', weight: -0.1e14 });
-      // e.toJson().should.eql('{"destination":"1","label":"label","source":0,"weight":-10000000000000}');
-    });
-
-    it('# should deep-stringify all the fields', () => {
-      // let source = { a: 1, b: [{ c: 'cLab' }, 4] };
-      // let dest = [1, 2, 3, [4, 5, 6]];
-      // let label = "undefined label"
-      // let weight = 1.1e4;
-      // let e = new EmbeddedEdge(source, dest, { label: label, weight: weight });
-      // e.toJson().should.eql('{"destination":["1","2","3","[\\\"4\\\",\\\"5\\\",\\\"6\\\"]"],"label":"undefined label","source":{"a":1,"b":["{\\\"c\\\":\\\"cLab\\\"}","4"]},"weight":11000}');
+      const e = new EmbeddedEdge(u, v, { weight: 12, label: 'edge', arcControlDistance: -32, isDirected: true });
+      JSON.parse(e.toJson()).should.eql({
+        source: { "label": ["u"], "weight": 1, "position": "[1,2]" },
+        destination: { "label": { "name": ["v"] }, "weight": 1, "position": "[1,2]" },
+        weight: 12,
+        arcControlDistance: -32,
+        directed: true,
+        label: "edge"
+      });
     });
   });
 
+  describe('fromJson()', () => {
+    const w = new EmbeddedVertex(['test', {'test': true}], new Point2D(2.5, 0.12345), { weight: 1.5, vertexRadius: 10 });
+    it('# applyed to the result of toJson, it should match source vertex', () => {
+      const e = new EmbeddedEdge(u, w, { weight: 12, label: 'long edge', arcControlDistance: -3.14, isDirected: true });
+      const e1 = EmbeddedEdge.fromJson(e.toJson());
+      e1.should.eql(e);
+      e1.source.equals(e.source).should.be.true();
+      e1.destination.equals(e.destination).should.be.true();
+      e1.label.should.eql(e.label);
+      e1.weight.should.eql(e.weight);
+      e1.arcControlDistance.should.eql(e.arcControlDistance);
+      e1.isDirected().should.eql(e.isDirected());
+    });
+  });
+
+
   describe('toSvg()', () => {
     it('# should return a valid svg', () => {
-      let v = new EmbeddedVertex("v", new Point(20, 20), {weight: 0.8})
-      let u = new EmbeddedVertex("u", new Point(120, 70), {weight: 2})
-      let edge = new EmbeddedEdge(u, v, {weight: 2, label: "Edge!"});
-      let edge2 = new EmbeddedEdge(v, u, {weight: 5, label: "test"});
+      let v = new EmbeddedVertex("v", new Point2D(20, 20), { weight: 0.8 })
+      let u = new EmbeddedVertex("u", new Point2D(120, 70), { weight: 2 })
+      let edge = new EmbeddedEdge(u, v, { weight: 2, label: "Edge!" });
+      let edge2 = new EmbeddedEdge(v, u, { weight: 5, label: "test" });
       console.log(edge.toSvg());
       console.log(edge2.toSvg());
       console.log(v.toSvg());
@@ -150,17 +173,17 @@ describe('Methods', () => {
     });
 
     it('# should return a valid svg 2 ', () => {
-      let v = new EmbeddedVertex("v", new Point(50, 170), {weight: 0.8})
-      let u = new EmbeddedVertex("u", new Point(320, 250), {weight: 2})
-      let edge = new EmbeddedEdge(u, v, {weight: 2, label: "Edge!", isDirected: true});
-      let edge2 = new EmbeddedEdge(v, u, {weight: 5, label: "test", isDirected: true});
-      let loop = new EmbeddedEdge(v, v, {weight: 5, label: "loop long", isDirected: false});
-      let loop2 = new EmbeddedEdge(u, u, {weight: 2, label: "loop", isDirected: true});
+      let v = new EmbeddedVertex("v", new Point2D(50, 170), { weight: 0.8 })
+      let u = new EmbeddedVertex("u", new Point2D(320, 250), { weight: 2 })
+      let edge = new EmbeddedEdge(u, v, { weight: 2, label: "Edge!", isDirected: true });
+      let edge2 = new EmbeddedEdge(v, u, { weight: 5, label: "test", isDirected: true });
+      let loop = new EmbeddedEdge(v, v, { weight: 5, label: "loop long", isDirected: false });
+      let loop2 = new EmbeddedEdge(u, u, { weight: 2, label: "loop", isDirected: true });
       console.log(v.toSvg());
       console.log(u.toSvg());
-      console.log(edge.toSvg({useArcs: true}));
+      console.log(edge.toSvg({ useArcs: true }));
       console.log(edge.toSvg());
-      console.log(edge2.toSvg({useArcs: true}));
+      console.log(edge2.toSvg({ useArcs: true }));
       console.log(loop.toSvg());
       console.log(loop2.toSvg());
     });
