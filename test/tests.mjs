768fb903780e66d4fa1e74f17616abcf7c2b5c26
mlarocca <marcellolarocca@gmail.com>
2020-04-19 23:59:05 +0200

    Run all tests

---
+++ test/tests.mjs
@@ -0,0 +1,18 @@
+import './common/hash.test.mjs';
+import './common/utils.test.mjs';
+
+import './disjointset/disjointset.test.mjs';
+
+import './dway_heap/dway_heap.test.mjs';
+
+import './geometric/cube.test.mjs';
+import './geometric/point.test.mjs';
+import './geometric/point2d.test.mjs';
+
+import './graph/vertex.test.mjs';
+import './graph/edge.test.mjs';
+import './graph/graph.test.mjs';
+
+import './graph/embedding/embedded_vertex.test.mjs';
+import './graph/embedding/embedded_edge.test.mjs';
+import './graph/embedding/embedding.test.mjs';
