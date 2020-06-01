558d4cd4b27780b28ddf562d43d93c9b7049fc30
mlarocca <marcellolarocca@gmail.com>
2020-06-01 15:57:42 +0200

    simulated annealing export nit

--- jsgraphs.mjs
+++ jsgraphs.mjs
@@ -6,7 +6,7 @@ import Embedding from './src/graph/embedding/embedding.mjs';
 import {isPlanar} from './src/graph/algo/planarity/kuratowski.mjs';
 import {minimumIntersectionsEmbedding} from './src/graph/algo/crossing_number/randomized_mcn.mjs';
 import {Permutations} from './src/graph/algo/combinatorial.mjs';
-import {simulatedAnnealing} from './src/graph/algo/simulated_annealing/simulated_annealing.mjs';
+import simulatedAnnealing from './src/graph/algo/simulated_annealing/simulated_annealing.mjs';
 import tsp from './src/graph/algo/simulated_annealing/tsp.mjs';
 
 import Point2D from './src/geometric/point2d.mjs';
