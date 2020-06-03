477d842dbbe64f678b693eb143fba27dedb258ed
Marcello La Rocca <mlarocca@users.noreply.github.com>
2020-06-03 20:57:48 +0200

    Simulated Annealing for Minimum Crossing Number (#38)
    * TSP with simulated annealing
    
    * simulated annealing export nit
    
    * Permutations docs
    
    * Simulated annealing + MCN

--- jsgraphs.mjs
+++ jsgraphs.mjs
@@ -4,10 +4,11 @@ import Vertex from './src/graph/vertex.mjs';
 import Edge from './src/graph/edge.mjs';
 import Embedding from './src/graph/embedding/embedding.mjs';
 import {isPlanar} from './src/graph/algo/planarity/kuratowski.mjs';
-import {minimumIntersectionsEmbedding} from './src/graph/algo/crossing_number/randomized_mcn.mjs';
+import {default as randomMce} from './src/graph/algo/random_sampling/mcn.mjs';
 import {Permutations} from './src/graph/algo/combinatorial.mjs';
 import simulatedAnnealing from './src/graph/algo/simulated_annealing/simulated_annealing.mjs';
 import tsp from './src/graph/algo/simulated_annealing/tsp.mjs';
+import {default as annealingMce} from './src/graph/algo/simulated_annealing/mcn.mjs';
 
 import Point2D from './src/geometric/point2d.mjs';
 
@@ -20,9 +21,14 @@ globalThis.jsgraphs = {
   Embedding: Embedding,
   algo: {
     isPlanar: isPlanar,
-    randomizedMinimumIntersectionsEmbedding: minimumIntersectionsEmbedding,
-    simulatedAnnealing: simulatedAnnealing,
+    randomSampling : {
+      minimumIntersectionsEmbedding: randomMce
+    },
+    simulatedAnnealing: {
+      simulatedAnnealing: simulatedAnnealing,
+      travelingSalesmanProblem: tsp,
+      minimumIntersectionsEmbedding: annealingMce
+    },
     Permutations: Permutations,
-    travelingSalesmanProblem: tsp
   }
 }
